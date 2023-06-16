171cb6ab655013c97b33f049644f02a1dcd52743
sunwoo <sunsense9@gmail.com>
2023-06-17 02:17:59 +0900

    baekjoon 2309 code edit

--- algorithm/baekjoon/code_plus/브루트 포스/2309_일곱 난쟁이.py
+++ algorithm/baekjoon/code_plus/브루트 포스/2309_일곱 난쟁이.py
@@ -13,13 +13,11 @@ arr = sorted(arr) # 리스트 오름차순 정렬
 spy_height_sum = sum(arr) - 100 # 일곱 난쟁이가 아닌 두 명의 키의 합
 first, second = 0, 0 # 난쟁이 두명의 키 초기화
 
-for i in arr: # 첫 번째 난쟁이
-    copy_arr = arr.copy()
-    copy_arr.remove(i) # 첫 번째 난쟁이를 뺀 리스트(중복 방지)
-    for j in copy_arr: # 두 번째 난쟁이
-        if i+j == spy_height_sum: # 두 난쟁이의 키의 합이 spy_height_sum인지 확인
+for i in range(len(arr)): # 첫 번째 난쟁이
+    for j in range(i+1, len(arr)): # 두 번째 난쟁이
+        if arr[i] + arr[j] == spy_height_sum: # 두 난쟁이의 키의 합이 spy_height_sum인지 확인
             # 맞으면 두 난쟁이의 키 저장
-            first, second = i, j
+            first, second = arr[i], arr[j]
 
 # 리스트에서 두 난쟁이를 삭제 후 나머지를 출력
 arr.remove(first)
